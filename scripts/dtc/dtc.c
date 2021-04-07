3c30b38da526275a2c9790bd55f2112fd7585099
Kristof Petho <kristof.petho@gmail.com>
2021-04-07 16:50:01 +0200

    dtc: be very quiet

--- scripts/dtc/dtc.c
+++ scripts/dtc/dtc.c
@@ -263,7 +263,10 @@ int main(int argc, char *argv[])
 			usage("unknown option");
 		}
 	}
-
+#if 1
+	if (!quiet) quiet++;
+#endif
+	
 	if (argc > (optind+1))
 		usage("missing files");
 	else if (argc < (optind+1))
