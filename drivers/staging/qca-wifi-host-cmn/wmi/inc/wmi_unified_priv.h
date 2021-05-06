71a00e52d99bd7241a2d5f9d1796ee0be3bce523
Park Ju Hyung <qkrwngud825@gmail.com>
2021-05-06 10:27:38 +0200

    qcacld: disable debugging bloat as much as possible
    The default defconfig enables a lot of debugging features.
    Disable those by referencing other perf defconfigs.
    
    Adapted to sm8350 by: Kristof Petho <kristof.petho@gmail.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/staging/qca-wifi-host-cmn/wmi/inc/wmi_unified_priv.h
+++ drivers/staging/qca-wifi-host-cmn/wmi/inc/wmi_unified_priv.h
@@ -126,6 +126,8 @@ struct wmi_ext_dbg_msg {
 };
 #endif /*WMI_EXT_DBG */
 
+#ifdef WMI_INTERFACE_EVENT_LOGGING
+
 #define wmi_alert(params...) QDF_TRACE_FATAL(QDF_MODULE_ID_WMI, ## params)
 #define wmi_err(params...) QDF_TRACE_ERROR(QDF_MODULE_ID_WMI, ## params)
 #define wmi_warn(params...) QDF_TRACE_WARN(QDF_MODULE_ID_WMI, ## params)
@@ -149,7 +151,6 @@ struct wmi_ext_dbg_msg {
 #define wmi_info_rl(params...) QDF_TRACE_INFO_RL(QDF_MODULE_ID_WMI, params)
 #define wmi_debug_rl(params...) QDF_TRACE_DEBUG_RL(QDF_MODULE_ID_WMI, params)
 
-#ifdef WMI_INTERFACE_EVENT_LOGGING
 /* wmi entry size */
 #ifndef WMI_DEBUG_ENTRY_MAX_LENGTH
 #define WMI_DEBUG_ENTRY_MAX_LENGTH (16)
@@ -342,6 +343,26 @@ enum WMI_RECORD_TYPE {
 	WMI_CMD = 1,
 	WMI_EVT = 2,
 };
+
+#else
+
+#define wmi_alert(params...) ((void)0)
+#define wmi_err(params...) ((void)0)
+#define wmi_warn(params...) ((void)0)
+#define wmi_info(params...) ((void)0)
+#define wmi_debug(params...) ((void)0)
+
+#define wmi_nofl_alert(params...) ((void)0)
+#define wmi_nofl_err(params...) ((void)0)
+#define wmi_nofl_warn(params...) ((void)0)
+#define wmi_nofl_info(params...) ((void)0)
+#define wmi_nofl_debug(params...) ((void)0)
+
+#define wmi_alert_rl(params...) ((void)0)
+#define wmi_err_rl(params...) ((void)0)
+#define wmi_warn_rl(params...) ((void)0)
+#define wmi_info_rl(params...) ((void)0)
+#define wmi_debug_rl(params...) ((void)0)
 
 #endif /*WMI_INTERFACE_EVENT_LOGGING */
 
